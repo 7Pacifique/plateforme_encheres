c4148e59dddfbdf2c946023685d8939d2b39e066
oziasgbesso26-prog <oziasgbesso26@gmail.com>
2026-04-22 20:27:59 +0100

    plateforme.py : structure de base et attributs

--- plateforme_encheres/models/plateforme.py
+++ plateforme_encheres/models/plateforme.py
@@ -7,3 +7,18 @@ Méthodes prévues : charger_donnees(), sauvegarder(), lancer()
 """
 
 # TODO Bloc 3 : implémenter la classe Plateforme
+
+
+import json
+import os
+from models.utilisateur import Utilisateur
+from models.objet import Objet
+from models.enchere import Enchere
+
+class Plateforme:
+
+    def __init__(self):
+        self.utilisateurs = {}
+        self.objets = []
+        self.encheres = []
+        self.utilisateur_connecte = None

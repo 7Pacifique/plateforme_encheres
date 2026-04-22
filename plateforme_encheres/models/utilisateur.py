f2e0705a543c4c70763cc1250cecb2b9adcd0c3b
oziasgbesso26-prog <oziasgbesso26@gmail.com>
2026-04-22 19:29:49 +0100

    feat(bloc3): ajout classe Utilisateur avec attributs de base

--- plateforme_encheres/models/utilisateur.py
+++ plateforme_encheres/models/utilisateur.py
@@ -7,3 +7,14 @@ Méthodes prévues : inscrire(), se_connecter(), crediter(), debiter(), to_dict(
 """
 
 # TODO Bloc 3 : implémenter la classe Utilisateur
+
+
+class Utilisateur:
+
+    def __init__(self, nom, email, mot_de_passe):
+        self.nom = nom
+        self.email = email
+        self.mot_de_passe = mot_de_passe
+        self.solde = 10000
+        self.historique = []
+        self.encheres_en_cours = set()

75caeaf9faaca6aaaeaa9b51cfcb152f4e9decde
oziasgbesso26-prog <oziasgbesso26@gmail.com>
2026-04-22 20:22:44 +0100

    enchere.py : creation classe Enchere avec ses attributs

--- plateforme_encheres/models/enchere.py
+++ plateforme_encheres/models/enchere.py
@@ -7,3 +7,14 @@ Méthodes prévues : placer_mise(), cloturer(), get_gagnant(), to_dict(), from_d
 """
 
 # TODO Bloc 3 : implémenter la classe Enchere
+
+
+from datetime import datetime
+
+class Enchere:
+
+    def __init__(self, objet_titre, encherisseur_email, montant):
+        self.objet_titre = objet_titre
+        self.encherisseur_email = encherisseur_email
+        self.montant = montant
+        self.date_heure = datetime.now().strftime("%d/%m/%Y %H:%M:%S")

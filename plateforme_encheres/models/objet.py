90ce9255dc9f6d9c16eb133dda8ed15840294e52
oziasgbesso26-prog <oziasgbesso26@gmail.com>
2026-04-22 20:06:43 +0100

    objet.py : mise en place de la classe avec ses champs

--- plateforme_encheres/models/objet.py
+++ plateforme_encheres/models/objet.py
@@ -7,3 +7,15 @@ Méthodes prévues : mettre_en_vente(), est_disponible(), to_dict(), from_dict()
 """
 
 # TODO Bloc 3 : implémenter la classe Objet
+
+class Objet:
+
+    def __init__(self, titre, description, prix_depart, vendeur_email):
+        self.titre = titre
+        self.description = description
+        self.prix_depart = prix_depart
+        self.vendeur_email = vendeur_email
+        self.meilleure_offre = prix_depart
+        self.meilleur_encherisseur = None
+        self.statut = "en_cours"
+        self.encherisseurs = set()
